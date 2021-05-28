5651788c07978242a53302a2a85bd58bc375a5f1
xNascimento13x <82427278+xNascimento13x@users.noreply.github.com>
2021-05-28 10:57:26 -0300

    Add files via upload

--- Game-com-Python.py
+++ Game-com-Python.py
@@ -1,5 +1,18 @@
 import pygame
 pygame.init ()
 
-janela = pygame.desplay.set_mode((800,600))
-pygame.display
+janela = pygame.display.set_mode((800,600))
+pygame.display.pygame.display.set_caption("game rodando")
+janela_aberta = True
+while janela_aberta:
+    pygame.time.delay(50)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            janela_aberta = False
+
+pygame.drw.pygame.draw.circle(janela,(0,255,0) , (400,300) , 50)
+
+pygame.display.update()
+
+pygame.quit
