10cd773179800cf12ba08561a250c2d182a8dd64
xNascimento13x <82427278+xNascimento13x@users.noreply.github.com>
2021-05-28 11:12:36 -0300

    Add files via upload

--- Game-com-Python.py
+++ Game-com-Python.py
@@ -1,8 +1,12 @@
 import pygame
 pygame.init ()
+x = 400
+y = 300
 
+velocidade = 5
 janela = pygame.display.set_mode((800,600))
 pygame.display.pygame.display.set_caption("game rodando")
+
 janela_aberta = True
 while janela_aberta:
     pygame.time.delay(50)
@@ -11,8 +15,16 @@ while janela_aberta:
         if event.type == pygame.QUIT:
             janela_aberta = False
 
-pygame.drw.pygame.draw.circle(janela,(0,255,0) , (400,300) , 50)
-
-pygame.display.update()
+            comandos = pygame.key.get_pressed()
+            if comandos [pygame.k_up] :
+                y-= velocidade
+            if comandos [pygame.k_DOWN] :
+                y+= velocidade
+            if comandos [pygame.k_RIGHT] :
+                X-= velocidade
+            if comandos [pygame.k_left] :
+                X+= velocidade
+    pygame.draw.circle(janela, (0,255,0) ,(x,y), (400,300) , 50)
+    pygame.display.update()
 
 pygame.quit
